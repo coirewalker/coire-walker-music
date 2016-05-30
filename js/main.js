46c5f5b2384a84034430f0e2084cc218ca96871a
whereiscoire <whereiscoire@gmail.com>
2016-05-30 18:28:21 -0400

    added mobile nav, bg image, basic styling

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-main.js
+$(document).ready(function() {
+
+	$(".ham-icon").click(function () {
+        $(".mobilenav").fadeToggle(500);
+        $(".top-menu").toggleClass("top-animate");
+        $(".mid-menu").toggleClass("mid-animate");
+        $(".bottom-menu").toggleClass("bottom-animate");
+    });
+    
+});
